b119342f34d89750b5175f705e9419116eaed3f1
Kat-Erina <nalchevanidzekaterina@gmail.com>
2024-03-12 13:17:03 +0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+import { useForm } from "react-hook-form";
+import "./App.css";
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  console.log(errors.name?.type);
+  console.log(errors.name?.message);
+
+  // console.log(useForm());
+  // console.log({ ...register }); - carielia
+  async function submitHandler(data) {
+    console.log(data);
+    console.log(data.name);
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit(submitHandler)}>
+        <label htmlFor="name">Name</label>
+        <input
+          className={`${
+            errors.name?.message
+              ? "border border-solid  border-rose-400"
+              : "border border-solid border-sky-500 "
+          }`}
+          type="text"
+          placeholder="name"
+          id="name"
+          {...register("name", {
+            required: "Name field is required",
+            minLength: {
+              value: 4,
+              message: "Min Length is 4",
+            },
+          })}
+        ></input>
+        <p>{errors.name?.message}</p>
+        <label htmlFor="email">Email</label>
+        <input
+          className={`${
+            errors.email?.message
+              ? "border border-solid  border-rose-400"
+              : "border border-solid border-sky-500 "
+          }`}
+          type="email"
+          placeholder="email"
+          id="email"
+          {...register("email", { required: "Email field is required" })}
+        ></input>
+        <p>{errors.email?.message}</p>
+        <label htmlFor="password">password</label>
+        <input
+          className={`${
+            errors.password?.message
+              ? "border border-solid  border-rose-400"
+              : "border border-solid border-sky-500 "
+          }`}
+          type="password"
+          placeholder="password"
+          id="password"
+          {...register("password", { required: "Password is required" })}
+        ></input>
+        <p>{errors.password?.message}</p>
+        <button type="submit">Submit</button>
+      </form>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
